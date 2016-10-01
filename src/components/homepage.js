5c73b3189b019ba715601349548556df3fa480fe
paulcarroll <pvcarroll@gmail.com>
2016-10-01 14:13:19 -0500

    homepage desktop alignment

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -8,11 +8,13 @@ export default class HomePage extends Component {
         <div className="container">
           <div className="lead">
             <img src={logo} alt="placeholder"/>
+          </div>
+          <div className="leadRight">
             <h1>ATX Permitting Review Check</h1>
             <h2>Get your residential express check.</h2>
+            <button className="btn">Get Started</button>
+            <p><a href="#">About</a></p>
           </div>
-          <button className="btn">Get Started</button>
-          <p><a href="#">About</a></p>
         </div>
       </main>
     );
